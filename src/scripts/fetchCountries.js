e765e3da9819a2cf590292918f4ac3e0fb33b0b8
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-30 04:24:02 +0300

    добавила  delay

--- src/scripts/fetchCountries.js
+++ src/scripts/fetchCountries.js
@@ -27,7 +27,7 @@ function onSearchCountry(e) {
             renderCountriesList(searchQuery);
         } else if (searchQuery.length > 10){
             const myError = error({
-                delay: 1500,
+                delay: 1000,
                 text:"Too many matches found. Pleas enter a more specific query"
                 });            
         }
